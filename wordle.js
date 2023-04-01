e77fea907219e83064735fa69ff92f63cdb20d2b
Janine <jlyn@cfa.harvard.edu>
2023-04-01 11:54:01 -0400

    Add files via upload

--- wordle.js
+++ wordle.js
@@ -3,20 +3,24 @@ function loadBoard() {
 	let game = new Game();
 	game.buildGameBoard();
 	game.buildKeyBoard();
-
+    game.getRandomWord();
 }
 function Game() {
 	this.debug = 0;
 	this.theWord = "";
     this.boardDim = [5, 6]; // dimension for Squares board
-    this.numGuesses = 1;    // track number of guesses
-    this.squares = [];      // stores an array of Square objects
-	this.keys = [];			// stores keyboard letters
+    this.numGuesses = 1;    // track number of guesses (same as number of rows)
 	this.currentSq=0;       // track which Square should be filled in
+	this.qwerty = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"];
+	this.colors = ["#86888a", "#cdb559", "#6aaa64", "#ffffff"]
+
+	//this.rowRanges= [ [0,4], [5,9], [10,14], [15,19], [20,24], [25,29] ];
+	this.start = 0;  // Start of row range
+	this.end = 4;    // End of row range
 
     this.buildGameBoard = buildGameBoard;
 	this.buildKeyBoard = buildKeyBoard;
-    this.createButton = createButton;
+    this.createSquare = createSquare;
 	this.createKeyButton = createKeyButton;
 	this.createFunctionButton = createFunctionButton;
     this.getRandomWord = getRandomWord;
@@ -25,9 +29,13 @@ function Game() {
 	this.addLetter = addLetter;
 	this.checkGuess = checkGuess;
 	this.moveBack = moveBack;
+	this.updateColors = updateColors;
 	this.setMessage = setMessage;
+	this.disableKeyboard = disableKeyboard;
+	this.addRestartButton = addRestartButton;
+	this.reloadBoard = reloadBoard;
 
-    this.getRandomWord();
+	this.setMessage("&nbsp;");
 }
 
 function buildGameBoard() {
@@ -41,18 +49,17 @@ function buildGameBoard() {
     this.boardDim.map(dim => len *= dim);
 
 	for (let ii = 0; ii < len; ii++) {
-		this.createButton(ii, grid);
-		this.squares.push( new Square() );
+		this.createSquare(ii, grid);
 	}
 	board.appendChild(grid);
 }
 
 function buildKeyBoard() {
-	let qwerty = ["QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"];
 	let keyboard = document.getElementById("keyboard")
 	let keygrid = document.createElement('div');
+	keygrid.id = "keygrid";
 
-	qwerty.forEach(row => {
+	this.qwerty.forEach(row => {
 		let letterArray = row.split("");
 
 		if (letterArray.includes("Z")) {
@@ -61,7 +68,6 @@ function buildKeyBoard() {
 
 		letterArray.forEach(letter => {
 			this.createKeyButton(letter, keygrid);
-			this.keys.push( new Key(letter) );
 		});
 
 		if (letterArray.includes("Z")) {
@@ -74,10 +80,10 @@ function buildKeyBoard() {
 	keyboard.appendChild(keygrid);
 }
 
-// create a button and attach it to the grid
-function createButton(numId, grid) {
+// create a square and attach it to the grid
+function createSquare(numId, grid) {
 	let button = document.createElement( "button");
-	button.id = "button" + numId;
+	button.id = "square" + numId;
 	button.className = "square";
 	button.type = "button";
 	button.innerHTML = "&nbsp;";
@@ -99,14 +105,16 @@ function createKeyButton(letter, grid) {
 	grid.appendChild(button);
 }
 
-
 function addLetter(id) {
-	this.setMessage("");
-	let letter = id.charAt(0);
+	this.setMessage("&nbsp;");
 
-	let sq = document.getElementById("button" + this.currentSq);
-	sq.innerText = letter;
-	this.currentSq++;
+	if ((this.currentSq >= this.start) && (this.currentSq <= this.end)) {
+		let letter = id.charAt(0);
+
+		let sq = document.getElementById("square" + this.currentSq);
+		sq.innerText = letter;
+		this.currentSq++;
+	}
 }
 function createFunctionButton(name, grid) {
 	let button = document.createElement( "button");
@@ -130,31 +138,103 @@ function createFunctionButton(name, grid) {
 }
 
 function checkGuess() {
-
-	if ( (this.currentSq) % 5 != 0) {
+	if (( (this.currentSq) % 5 !== 0 ) || (this.currentSq === this.start) ){
 		setMessage("Not enough letters");
 	} else {
 		let guess = "";
-		for (index = this.currentSq-5; index < this.currentSq; index++) {
-			guess += document.getElementById("button" + index).innerText;
+		let index;
+
+		for (index = this.start; index <= this.end; index++) {
+			guess += document.getElementById("square" + index).innerText;
 		}
-		console.log(guess);
+
+		// check if User's guess is a valid word
 		let isValid = this.validateGuess(guess);
 
-		// check if guess is a match to the word
+		// if not valid, print message "Not in word list"
+		if (!isValid) {
+			this.setMessage("Not in word list");
+			return;
+		}
 
-		// if not update colors
+		// if valid, update colors
+		this.updateColors(guess);
+		let done = ((guess === this.theWord) || (this.numGuesses === 6));
+
+		// check if guess is a match to the word
+		if (done) {
+			if (guess === this.theWord) {
+				this.setMessage("Congratulations!");
+			} else {
+				this.setMessage("The word is <b>" + this.theWord + "</b>");
+			}
+
+			this.disableKeyboard();
+			this.addRestartButton();
+			return;
+		}
 
 		// continue to next guess
+		this.numGuesses++;
 
+		// update the row ranges
+		this.start += 5;
+		this.end += 5;
+	}
+}
+
+function updateColors(guess) {
+	let index = 0;
+	let wordArray = this.theWord.split("");
+	let guessArray= guess.split("");
+	let results = [];
+
+	guessArray.forEach( letter => {
+		if (letter === wordArray[index]) {
+			// if letter is in the correct place, turn green
+			results.push(2);
+		} else {
+			if (wordArray.includes(letter)) {
+				// if letter is in the word but not the correct place, turn yellow
+				results.push(1);
+			} else {
+				// if letter is not in the word at all, turn gray
+				results.push(0);
+			}
+		}
+		index++;
+	});
+
+	let btn, sq;
+	let sqIndex = this.start;
+	let rgbGreen = "rgb(106, 170, 100)";
+	for (index = 0; index < 5; index++) {
+		// update keyboard letter color
+		btn = document.getElementById(guessArray[index]+"button");
+		btn.style.color = this.colors[3];
+
+		// if button is green, do not update the color
+		if (btn.style.backgroundColor !== rgbGreen) {
+			btn.style.backgroundColor = this.colors[results[index]];
+		}
+
+		// update wordle square color
+		sq = document.getElementById("square"+sqIndex);
+		sq.style.color = this.colors[3];
+		sq.style.backgroundColor = this.colors[results[index]];
+		sqIndex++;
 	}
 
 }
 function moveBack() {
-	console.log(this.currentSq);
-	if (this.currentSq > 0) {
-		this.currentSq--;
-		let sq = document.getElementById("button" + this.currentSq);
+	this.setMessage("&nbsp;");
+
+	if ((this.currentSq >= this.start) && (this.currentSq-1 <= this.end)) {
+		if (this.currentSq !== this.start) {
+			this.currentSq--;
+		}
+
+		let sq = document.getElementById("square" + this.currentSq);
 		sq.innerHTML = "&nbsp;";
 	}
 }
@@ -168,19 +248,80 @@ async function getRandomWord() {
 }
 
 function setWord(word) {
-	this.theWord = word;
+	this.theWord = word.toUpperCase();
 	console.log(this.theWord);
 }
 function validateGuess( guess ){
 
 	let req = new XMLHttpRequest();
-	req.open("GET", "https://api.dictionaryapi.dev/api/v2/entries/en/"+guess, false);
+	req.open("GET", "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + guess +
+			"?key=22b6c8ea-5808-466e-bf2f-146146bb7b4b", false);
 	req.send(null);
-	if(req.readyState == 4)  {
+	if(req.readyState === 4)  {
+		let	reqJSON = JSON.parse(req.responseText);
 
-		console.log(req.responseText);
+		// valid words return an object
+		// invalid words return an array or undefined
+		return (typeof reqJSON[0] === 'object');
 	}
 }
+
+// turn off hover and disable all buttons on the keyboard after the game has ended
+function disableKeyboard() {
+	let btn;
+	this.qwerty.forEach(row => {
+		let letterArray = row.split("");
+		letterArray.forEach(letter => {
+			btn = document.getElementById(letter + "button");
+			btn.disabled = true;
+			btn.style.pointerEvents = "none";
+		});
+    });
+
+	btn = document.getElementById("ENTER");
+	btn.disabled = true;
+	btn.style.pointerEvents = "none";
+
+	btn = document.getElementById("BACK");
+	btn.disabled = true;
+	btn.style.pointerEvents = "none";
+}
+
+// after the game has ended, add a button below the
+// game board allowing the user to reset the board and play another game
+function addRestartButton() {
+    let restart = document.getElementById("restart");
+    let btn = document.createElement( "button");
+    btn.type = "button";
+    btn.id = "playAgain";
+    btn.style.fontSize = "20px";
+    btn.innerText = "Play Again";
+    btn.setAttribute("class", "w3-button w3-black w3-round-large")
+    btn.addEventListener ('click', this.reloadBoard);
+    restart.appendChild(btn);
+}
+
+// reload board and start a new game
+function reloadBoard() {
+    // remove the old board
+    let div = document.getElementById("grid");
+    div.parentNode.removeChild(div);
+
+    // remove the old keyboard
+    div = document.getElementById("keygrid");
+    div.parentNode.removeChild(div);
+
+    // remove the 'Play Again' button
+    div = document.getElementById("playAgain");
+    div.parentNode.removeChild(div);
+
+	// clear banner
+	setMessage(" ");
+
+    // load a fresh game board
+    loadBoard();
+}
+
 /*
 function setDebugMode(){
 	let banner = document.getElementById("banner");
@@ -197,25 +338,7 @@ function setDebugMode(){
 }
 */
 
-
 function setMessage(msg)
 {
     document.getElementById("banner").innerHTML=msg;
-}
-
-
-function Square() {
-	this.input = "";
-	this.isEmpty = isEmpty;
-}
-
-function isEmpty() {
-    return this.input === "" ? true : false;
-
-}
-
-function Key(inLetter) {
-	this.letter = inLetter;
-	this.isTaken = false;
-
 }
